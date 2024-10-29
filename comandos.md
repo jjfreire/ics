5caddbc7641744c00fd70a6c140920d0d9f0e389
Juanjo <j.j.freire@udc.es>
2024-10-29 18:04:22 +0100

    ejercicio1

---
+++ comandos.md
@@ -0,0 +1,19 @@
+### Lanzar docker
+```bash
+docker run --hostname=quickstart.cloudera --privileged=true -t -i --cpus=12 -v /home/juanjo/repos/muei/ics/ejercicio1:/home/cloudera/ejercicio1 --publish-all=true -p 7180 cloudera/quickstart /usr/bin/docker-quickstart
+```
+
+### Lanzar HDFS (dentro de docker)
+```bash
+hdfs dfs -put sources
+```
+
+### Hacer un ls en el HDFS
+```bash
+hdfs dfs -ls sources 
+```
+
+### Lanzar código en Hadoop
+```bash
+/usr/bin/hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming-2.6.0-cdh5.7.0.jar -D mapreduce.job.reduces=2 -input sources -output output_dir -mapper mapper.py -reducer reducer.py -file mapper.py -file reducer.py
+```
---
+++ ejercicio1/reducer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import sys
+
+city_id = None
+max_temp = None
+min_temp = None
+
+for line in sys.stdin:
+    line = line.strip()
+    current_city_id, current_max_temp, current_min_temp = line.split('\t')
+    try:
+        current_max_temp = float(current_max_temp)
+        current_min_temp = float(current_min_temp)
+    except ValueError:
+        continue
+
+    if city_id == current_city_id:
+        if max_temp is None or current_max_temp > max_temp:
+            max_temp = current_max_temp
+        if min_temp is None or current_min_temp < min_temp:
+            min_temp = current_min_temp
+    else:
+        if city_id is not None:
+            print '%s\t%s\t%s' % (current_city_id, max_temp, min_temp)
+        city_id = current_city_id
+        max_temp = current_max_temp
+        min_temp = current_min_temp
+
+if city_id == current_city_id:
+    print '%s\t%s\t%s' % (current_city_id, max_temp, min_temp)
