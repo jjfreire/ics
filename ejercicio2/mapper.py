92a36802474c1ffa342d0192a8187e8fb9c9200e
AlejandroArizaUDC <alejandro.ariza@udc.es>
2024-10-29 19:42:54 +0100

    Mapper v1

--- ejercicio2/mapper.py
+++ ejercicio2/mapper.py
@@ -8,7 +8,7 @@ user = os.environ['mapreduce_map_input_file'].split('/')[-1].split('.')[0][3:]
 for line in sys.stdin:
     line = line.strip()
     fields = line.split()
-    if fields[3].endswith('.ps'):
+    if fields[3].endswith('.ps"'):
         print '%s\t%s\t%d' % (fields[3], user, 1)
     else:
         print '%s\t%s\t%d' % (fields[3], user, 0)
