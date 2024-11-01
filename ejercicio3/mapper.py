852202793437e12f873dc445cbdeb77d262a2ecc
AlejandroArizaUDC <alejandro.ariza@udc.es>
2024-11-01 20:11:03 +0100

    arreglo prints mapper ej3

--- ejercicio3/mapper.py
+++ ejercicio3/mapper.py
@@ -14,8 +14,8 @@ for line in sys.stdin:
         continue
 
     line = line.strip()
-    fields = line.split()
+    fields = line.split(';')
     if wine_type.endswith('red.csv'):
-        print '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s' % ('red', fields[0], fields[1], fields[2], fields[3], fields[4], fields[5], fields[6], fields[7], fields[8], fields[9], fields[10], fields[11])
+        print '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s' % ('red', fields[0], fields[1], fields[2], fields[3], fields[4], fields[5], fields[6], fields[7], fields[8], fields[9], fields[10], fields[11])
     else:
-        print '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s' % ('white', fields[0], fields[1], fields[2], fields[3], fields[4], fields[5], fields[6], fields[7], fields[8], fields[9], fields[10], fields[11])
+        print '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s' % ('white', fields[0], fields[1], fields[2], fields[3], fields[4], fields[5], fields[6], fields[7], fields[8], fields[9], fields[10], fields[11])
